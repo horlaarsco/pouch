b5ed36ee1a89d8d6be91d0b5ec82f546bdce6808
Darey <horla@Dareys-MacBook-Pro.local>
2020-06-28 04:48:57 +0100

    anoda

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var PouchDB = require("pouchdb");
 var express = require("express");
 var app = express();
+var cors = require("cors");
+
 require("dotenv").config();
+app.use(cors());
 
 app.use("/", require("express-pouchdb")(PouchDB));
 
